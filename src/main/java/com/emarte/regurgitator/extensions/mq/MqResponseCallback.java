d17d0450b242e1106fa17c41e47c4819057acb35
talmeym <mtalmey@gmail.com>
2016-03-16 13:48:33 +0000

    tidied, and overrides destination in mq response callback

--- src/main/java/com/emarte/regurgitator/extensions/mq/MqResponseCallback.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/MqResponseCallback.java
@@ -7,24 +7,33 @@ import javax.jms.*;
 
 import static com.emarte.regurgitator.core.Log.getLog;
 import static com.emarte.regurgitator.core.StringType.stringify;
+import static com.emarte.regurgitator.extensions.mq.ExtensionsMqConfigConstants.JMS_DESTINATION;
+import static com.emarte.regurgitator.extensions.mq.ExtensionsMqConfigConstants.RESPONSE_METADATA_CONTEXT;
 import static com.emarte.regurgitator.extensions.mq.MessageResponseUtil.applyResponseData;
 
 public class MqResponseCallback implements ResponseCallBack {
 	private static final Log log = getLog(MqResponseCallback.class);
 	private final MqMessagingSystem mqMessagingSystem;
-	private final String outputQueue;
+	private final String defaultOutputQueue;
 
-	public MqResponseCallback(MqMessagingSystem mqMessagingSystem, String outputQueue) {
+	public MqResponseCallback(MqMessagingSystem mqMessagingSystem, String defaultOutputQueue) {
 		this.mqMessagingSystem = mqMessagingSystem;
-		this.outputQueue = outputQueue;
+		this.defaultOutputQueue = defaultOutputQueue;
 	}
 
 	@Override
 	public void respond(Message message, Object value) {
 		try {
 			log.debug("Processing callback");
+			Parameter destinationParameter = message.getContextValue(new ContextLocation(RESPONSE_METADATA_CONTEXT, JMS_DESTINATION));
+			String destination = destinationParameter != null ? stringify(destinationParameter.getValue()) : defaultOutputQueue;
+
+			if(destinationParameter != null) {
+				log.debug("Overriding jms destination with parameter value '" + destination + "'");
+			}
+
 			QueueSession session = mqMessagingSystem.getConnection().createQueueSession(false, javax.jms.Session.AUTO_ACKNOWLEDGE);
-			QueueSender sender = session.createSender(session.createQueue(outputQueue));
+			QueueSender sender = session.createSender(session.createQueue(destination));
 			javax.jms.TextMessage jmsMessage = mqMessagingSystem.createTextMessage();
 
 			log.debug("Applying message data to jms message");
