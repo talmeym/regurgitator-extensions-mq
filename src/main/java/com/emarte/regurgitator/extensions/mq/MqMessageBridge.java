aa100583e43ec65eeaa580e191482cc3ab3ba197
talmeym <mtalmey@gmail.com>
2017-09-19 16:00:14 +0100

    tidied

--- src/main/java/com/emarte/regurgitator/extensions/mq/MqMessageBridge.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/MqMessageBridge.java
@@ -5,16 +5,20 @@ import com.emarte.regurgitator.core.*;
 import javax.jms.*;
 import javax.jms.Session;
 
+import static javax.jms.Session.AUTO_ACKNOWLEDGE;
+
 public class MqMessageBridge {
 	private static Log log = Log.getLog(MqMessageBridge.class);
 
 	public MqMessageBridge(MqMessagingSystem mqMessagingSystem, String inputDestination, String outputDestination, Regurgitator regurgitator) throws JMSException, RegurgitatorException {
 		log.debug("Creating message consumer");
 		Connection connection = mqMessagingSystem.getConnection();
-		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Session session = connection.createSession(false, AUTO_ACKNOWLEDGE);
 		MessageConsumer consumer = session.createConsumer(mqMessagingSystem.createDestination(inputDestination));
+
 		log.debug("Setting up message listener");
 		consumer.setMessageListener(new RegurgitatorMessageListener(regurgitator, new MqResponseCallback(mqMessagingSystem, outputDestination)));
+
 		log.debug("Awaiting jms messages");
 	}
 }
